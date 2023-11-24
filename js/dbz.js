6dea95a640269aa8ef0a60ef3305db5d20abdd50
Akzaatz <akzaatz@protonmail.com>
2023-11-24 22:48:26 +0100

    Save6

--- js/dbz.js
+++ js/dbz.js
@@ -8,3 +8,25 @@ gaz.addEventListener("click", () => {
 memo.addEventListener("click", () => {
   window.location.href = "memo.html";
 });
+
+// async function getworks() {
+//   const response = await fetch("http://localhost:3000/works");
+//   console.log(response);
+// }
+
+// getworks();
+
+let form = document.querySelector("form");
+let baliseNom = document.getElementById("nom");
+
+function verifierChamp(balise) {
+  if (balise.value === "") {
+    balise.classList.add("error");
+  } else {
+    balise.classList.remove("error");
+  }
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    verifierChamp(baliseNom);
+  });
+}
